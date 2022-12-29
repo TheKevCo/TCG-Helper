dae34c9d63eebec96bd12e400f8f47d59374e074
Kevin Colon <koak1995@gmail.com>
2022-12-28 20:28:14 -0500

    Version 1.0 Commit. Deck Pricing to be added in later commit.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_bootstrap import Bootstrap
 from datetime import datetime
 from bs4 import BeautifulSoup
